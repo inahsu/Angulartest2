186ef95ddbc4fe8df5e3556490a578527f7a71c2
Hsin Chen <ben.snow.heart@gmail.com>
2021-10-27 00:45:46 +0800

    增加sidebar

--- src/app/component/tab/tab.component.ts
+++ src/app/component/tab/tab.component.ts
@@ -8,8 +8,20 @@ import { Component } from '@angular/core';
 export class TabComponent {
 
   section: Array<any> = [];
+  openSideBar:boolean = true;
+  oriSize:Number=100;
+  openSize:Number=500;
+
+  unit:string="px";
+  sideBarSize:string="100px";
 
   constructor() {
-
+    this.sideBarSize = this.openSideBar? this.openSize + this.unit:this.oriSize + this.unit;
   }
+
+  changeSize(){
+    this.openSideBar = !this.openSideBar;
+    this.sideBarSize = this.openSideBar? this.openSize + this.unit:this.oriSize + this.unit;
+  }
+
 }
